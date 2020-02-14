4110952b831053bb37341d3cc851cf22b9722b15
Nikita Kayuzhnyy <nknik204@icloud.com>
2020-02-14 13:26:02 +0200

    Creating app

---
+++ Checklist/ChecklistItem.swift
@@ -0,0 +1,19 @@
+//
+//  ChecklistItem.swift
+//  Checklist
+//
+//  Created by Nikita Kalyuzhniy on 2/14/20.
+//  Copyright © 2020 Nikita Kalyuzhniy. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+    
+}
---
+++ Checklist/ChecklistViewController.swift
@@ -0,0 +1,154 @@
+//
+//  ChecklistViewController.swift
+//  Checklist
+//
+//  Created by Nikita Kalyuzhniy on 2/13/20.
+//  Copyright © 2020 Nikita Kalyuzhniy. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    
+    var items: [ChecklistItem]
+    
+    required init? (coder aDecoder: NSCoder) {
+        
+        items = [ChecklistItem]()
+        
+        let row0item = ChecklistItem()
+        row0item.text = "Walk the dog"
+        row0item.checked = false
+        items.append(row0item)
+        
+        let row1item = ChecklistItem()
+        row1item.text = "Eat ice cream"
+        row1item.checked = false
+        items.append(row1item)
+        
+        let row2item = ChecklistItem()
+        row2item.text = "Soccer practice"
+        row2item.checked = false
+        items.append(row2item)
+        
+        let row3item = ChecklistItem()
+        row3item.text = "Learn iOS development"
+        row3item.checked = false
+        items.append(row3item)
+        
+        let row4item = ChecklistItem()
+        row4item.text = "Brush my teeth"
+        row4item.checked = false
+        items.append(row4item)
+        
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    
+    //    override func numberOfSections(in tableView: UITableView) -> Int {
+    //
+    //        return 1
+    //    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            
+            let item = items[indexPath.row]
+            
+            item.toggleChecked()
+            
+            configureCheckmark(for: cell, with: item)        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        let item = items[indexPath.row]
+        label.text = item.text
+        
+        configureCheckmark(for: cell, with: item)
+        configureText(for: cell, with: item)
+        return cell
+    }
+    
+    func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
+                
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+    
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        label.text = item.text
+    }
+    
+
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
