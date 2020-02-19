9e89bee5545b110dfaa1b496bafb75cad12f13bd
Nikita Kayuzhnyy <nknik204@icloud.com>
2020-02-19 13:18:32 +0200

    Constraints & Code cleaning

--- Checklist/AllListsViewController.swift
+++ Checklist/AllListsViewController.swift
@@ -34,13 +34,7 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     }
     
     func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist) {
-        
-//        if let index = dataModel.lists.firstIndex(of: checklist) {
-//            let indexPath = IndexPath(row: index, section: 0)
-//            if let cell = tableView.cellForRow(at: indexPath) {
-//                cell.textLabel!.text = checklist.name
-//            }
-//        }
+
         dataModel.sortChecklists()
         tableView.reloadData()
         dismiss(animated: true, completion: nil)
--- Checklist/Supporting Files/AppDelegate.swift
+++ Checklist/Supporting Files/AppDelegate.swift
@@ -19,24 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIWindowSceneDelegate, UN
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-//        let center = UNUserNotificationCenter.current()
-//        center.requestAuthorization(options: [.alert, .sound]) {
-//            granted, error in if granted {
-//                print("We have permission")
-//            } else {
-//                print("Permission denied")
-//            }
-//        }
-//        let content = UNMutableNotificationContent()
-//        content.title = "Hello!"
-//        content.body = "I am a local notification"
-//        content.sound = UNNotificationSound.default
-//
-//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
-//
-//        let request = UNNotificationRequest(identifier: "MyNotification", content: content, trigger: trigger)
-//        center.add(request)
+
         let center = UNUserNotificationCenter.current()
         center.delegate = self
         return true
@@ -45,16 +28,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIWindowSceneDelegate, UN
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler competionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
